9309322b1ddd695af51e6e2c5194a68e2ee16f46
yoshi750 <146502759+yoshi750@users.noreply.github.com>
2023-10-24 23:13:47 +0900

    Add files via upload

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,12 +1,12 @@
 const holiday = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
 
-for (let i = 0; i <= 15; i++ )
+for (let i = 1 - 1; i < 16; i++ )
 
 console.log(holiday[i])
 
 let num = -1;
 
-while (num <= 15) {
+while (num < 15) {
     num++;
     console.log(holiday[num])
 }
